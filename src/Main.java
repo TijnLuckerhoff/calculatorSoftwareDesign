25486d4fac8aff81c1cc8de4e041d0798c22301e
TijnLuckerhoff <luck0002@hz.nl>
2024-11-20 11:53:29 +0100

    another commit 2

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(10, 20));
+        System.out.println(calculator.subtract(30, 20));
+        System.out.println(calculator.multiply(3, 20));
+        System.out.println(calculator.divide(40, 5));
+        System.out.println(calculator.square(2));
     }
 }
